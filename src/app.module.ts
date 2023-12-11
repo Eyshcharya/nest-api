0ff78702305fd7439d06c4e4b67d67b57cb27f45
Eyshcharya <eyshcharya@gmail.com>
2023-12-11 11:42:39 +0530

    Imported new modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
+import { ArticleModule } from './article/article.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
+import { ProfileModule } from './profile/profile.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    AuthModule,
+    ArticleModule,
+    PrismaModule,
+    JwtModule,
+    UserModule,
+    ProfileModule,
+  ],
 })
 export class AppModule {}
