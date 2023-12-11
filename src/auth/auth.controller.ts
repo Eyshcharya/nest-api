9c3b7d65a367309749961d70e5552ca9b5503461
Eyshcharya <eyshcharya@gmail.com>
2023-12-11 11:44:46 +0530

    Implemented controller logic for authentication

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { LoginDto, RegisterDto } from './dto';
+import { AuthService } from './auth.service';
 
-@Controller('auth')
-export class AuthController {}
+@Controller('users')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post()
+  register(@Body() dto: RegisterDto) {
+    return this.authService.register(dto);
+  }
+
+  @Post('login')
+  login(@Body() dto: LoginDto) {
+    return this.authService.login(dto);
+  }
+}
