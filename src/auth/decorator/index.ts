67a5144f68464e1918f19080279e94b6f3ca38fc
Eyshcharya <eyshcharya@gmail.com>
2023-12-11 11:50:40 +0530

    Implemented get profile decorator

---
+++ src/auth/decorator/index.ts
@@ -0,0 +1,13 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+
+export const GetUser = createParamDecorator(
+  // get the data passed, get execution context
+  (data: string | undefined, ctx: ExecutionContext) => {
+    const request: Express.Request = ctx.switchToHttp().getRequest();
+
+    if (data) {
+      return request.user[data];
+    }
+    return request.user;
+  },
+);
