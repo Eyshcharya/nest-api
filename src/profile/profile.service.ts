79cbd497acfcf35e9d0994930f28746fca12ad24
Eyshcharya <eyshcharya@gmail.com>
2023-12-13 04:08:47 +0530

    Modifications

--- src/profile/profile.service.ts
+++ src/profile/profile.service.ts
@@ -1,54 +1,88 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadGatewayException,
+  BadRequestException,
+  HttpException,
+  HttpStatus,
+  Injectable,
+  NotFoundException,
+  UnprocessableEntityException,
+} from '@nestjs/common';
+import { User } from '@prisma/client';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class ProfileService {
   constructor(private prisma: PrismaService) {}
 
-  // GET PROFILE
+  // //* GET PROFILE
   async getProfile(userName: string) {
-    // check if the current user follow the targeted user
-    const profile = await this.prisma.user.findFirst({
-      where: {
-        userName: userName,
-      },
-      select: {
-        userName: true,
-        bio: true,
-        image: true,
-      },
-    });
-    if (profile) {
+    try {
+      // find user
+      const profile = await this.prisma.user.findFirst({
+        where: {
+          userName: userName,
+        },
+        select: {
+          userName: true,
+          bio: true,
+          image: true,
+        },
+      });
+
+      if (!profile) {
+        // return 404
+        throw new NotFoundException('Resource Not Found');
+      }
+
+      // return profile
       return { profile: { ...profile, following: false } };
+    } catch (error) {
+      // return error
+      if (
+        error instanceof PrismaClientKnownRequestError &&
+        error.code === 'P2002'
+      ) {
+        throw new NotFoundException('Resource Not Found');
+      } else {
+        throw error;
+      }
     }
-    return { message: 'user does not exist' };
   }
 
   // FOLLOW USER
-  async followUser(userId: number, targetedUserName: string) {
-    // check if the current user follow the targeted user
-    const isFollowing = await this.prisma.user.findFirst({
-      where: {
-        id: userId,
-        following: {
-          has: targetedUserName,
-        },
-      },
-    });
-
+  async checkUser(userId: number, targetedUserName: string) {
+    // get targetProfile
     const targetedProfile = await this.prisma.user.findFirst({
       where: {
         userName: targetedUserName,
       },
       select: {
+        id: true,
         userName: true,
         bio: true,
         image: true,
       },
     });
 
+    if (!targetedProfile) {
+      // return 404
+      throw new NotFoundException('Resource Not Found');
+    }
+
+    // check if the current user follow the targeted user
+    const isFollowing = await this.prisma.user.findFirst({
+      where: {
+        id: userId,
+        followingUsers: {
+          has: targetedProfile.id,
+        },
+      },
+    });
+
     if (isFollowing) {
       // return the profile
+      delete targetedProfile.id;
       return { profile: { ...targetedProfile, following: true } };
     } else {
       // follow the targeted user
@@ -58,16 +92,19 @@ export class ProfileService {
             id: userId,
           },
           data: {
-            following: {
-              push: targetedUserName,
+            followingUsers: {
+              push: targetedProfile.id,
             },
           },
         });
         // return the profile
         if (updatedProfile) {
+          delete targetedProfile.id;
           return { profile: { ...targetedProfile, following: true } };
         } else {
-          return { message: 'Failed to follow the user' };
+          return new UnprocessableEntityException(
+            `Failed to follow user: ${targetedUserName}`,
+          );
         }
       } catch (error) {
         throw new Error(error);
@@ -77,29 +114,36 @@ export class ProfileService {
 
   // UNFOLLOW USER
   async unfollowUser(userId: number, targetedUserName: string) {
-    // check if the current user follow the targeted user
-    const isFollowing = await this.prisma.user.findFirst({
-      where: {
-        id: userId,
-        following: {
-          has: targetedUserName,
-        },
-      },
-      select: {
-        following: true,
-      },
-    });
-
     const targetedProfile = await this.prisma.user.findFirst({
       where: {
         userName: targetedUserName,
       },
       select: {
+        id: true,
         userName: true,
         bio: true,
         image: true,
       },
     });
+
+    if (!targetedProfile) {
+      // return 404
+      throw new NotFoundException('Resource Not Found');
+    }
+
+    // check if the current user follow the targeted user
+    const isFollowing = await this.prisma.user.findFirst({
+      where: {
+        id: userId,
+        followingUsers: {
+          has: targetedProfile.id,
+        },
+      },
+      select: {
+        followingUsers: true,
+      },
+    });
+
     if (isFollowing) {
       try {
         // unfollow the targeted user
@@ -108,24 +152,28 @@ export class ProfileService {
             id: userId,
           },
           data: {
-            following: {
-              set: isFollowing.following.filter(
-                (user) => user !== targetedUserName,
+            followingUsers: {
+              set: isFollowing.followingUsers.filter(
+                (user) => user !== targetedProfile.id,
               ),
             },
           },
         });
         if (updatedProfile) {
           // return the profile
+          delete targetedProfile.id;
           return { profile: { ...targetedProfile, following: false } };
         } else {
-          return { message: 'Failed to unfollow the user' };
+          return new UnprocessableEntityException(
+            `Failed to unfollow user: ${targetedUserName}`,
+          );
         }
       } catch (error) {
         throw new Error(error);
       }
     } else {
       // return the profile
+      delete targetedProfile.id;
       return { profile: { ...targetedProfile, following: false } };
     }
   }
